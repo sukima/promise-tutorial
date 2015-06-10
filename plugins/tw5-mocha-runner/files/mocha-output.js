a894b7f304466c3e76c79d75fec17d4ba8a068e3
Devin Weaver <suki@tritarget.org>
2015-06-09 21:08:17 -0400

    Add mocha-output widget
    This allows the previous specs to stay as is and not reset when the
    output tiddler is de-rendered. Uses a static variable to keep a
    reference to the one and only mocha node.

---
+++ plugins/tw5-mocha-runner/files/mocha-output.js
@@ -0,0 +1,52 @@
+/*\
+title: $:/plugins/sukima/tw5-mocha-runner/mocha-output.js
+type: application/javascript
+module-type: widget
+
+Run the specs and output the result
+\*/
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+(function() {
+'use strict';
+
+// This is a browser only script. Bail out on node.
+if (!$tw.browser) { return; }
+
+var Widget = require('$:/core/modules/widgets/widget.js').widget;
+
+// #mocha needs to be a static varibable that is reusable. Even though it gets
+// appended and removed as the widget is shown.
+var mochaDomNode = document.createElement('div');
+mochaDomNode.setAttribute('id', 'mocha');
+mochaDomNode.appendChild(
+  document.createTextNode('Select an example to run the specs.')
+);
+
+function mochaIsDetached() {
+  return (document.getElementById('mocha') == null);
+}
+
+var MochaOutputWidget = function(parseTreeNode, options) {
+  this.initialise(parseTreeNode, options);
+};
+
+MochaOutputWidget.prototype = new Widget();
+
+MochaOutputWidget.prototype.render = function(parent, nextSibling) {
+  this.parentDomNode = parent;
+  this.computeAttributes();
+  if (mochaIsDetached()) {
+    parent.appendChild(mochaDomNode);
+    this.domNodes.push(mochaDomNode);
+  }
+};
+
+MochaOutputWidget.prototype.refresh = function(changedTiddlers) {
+  return false;
+};
+
+exports['mocha-output'] = MochaOutputWidget;
+
+})();
